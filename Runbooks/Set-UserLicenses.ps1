3998660d83585b8a3fa301667531b323dfc74057
Brian Farnhill <brfarn@microsoft.com>
2017-02-15 09:47:36 +1000

    Formatting fixes

--- Runbooks/Set-UserLicenses.ps1
+++ Runbooks/Set-UserLicenses.ps1
@@ -6,26 +6,34 @@ $cred = Get-AutomationPSCredential -Name $azureCredentialName
 Connect-MsolService -Credential $cred
 
 $users = get-msoluser -all -DomainName $domainName
-$groups = get-msolgroup | ?{$_.displayname -like "LICENSE*"}
-Foreach ($group in $groups) {
+$groups = get-msolgroup | Where-Object { 
+    $_.displayname -like "LICENSE*" 
+}
+
+foreach ($group in $groups) {
     $members = Get-MsolGroupMember -GroupObjectId $group.ObjectId
-    Foreach ($user in $users) {
-        If ($members.emailaddress -contains $user.userprincipalname) {
+    foreach ($user in $users) {
+        if ($members.emailaddress -contains $user.userprincipalname) 
+        {
             # check to see if license needs to be assigned
-            If ($user.licenses.accountskuid -notcontains $group.description) {
+            if ($user.licenses.accountskuid -notcontains $group.description) 
+            {
                 # NEED TO ADD LICENSE
                 # Check for Usage Location Set First          
                 if (!$user.UsageLocation) {
                     Set-MsolUser $user.UserPrincipalName $user.UserPrincipalName -UsageLocation $Location
                 }
                 Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -AddLicenses $group.description
-                Write-Host "$($user.userprincipalname) - $($group.description) License has been added"
+                Write-Output "$($user.userprincipalname) - $($group.description) License has been added"
             }
-        } else {
+        } 
+        else 
+        {
             # check to see if license needs to be remove
-            If ($user.licenses.accountskuid -contains $group.description) {
+            if ($user.licenses.accountskuid -contains $group.description) 
+            {
                 # LICENSE ALREADY THERE
-                Write-Host "$($user.userprincipalname) - $($group.description) License has been removed"
+                Write-Output "$($user.userprincipalname) - $($group.description) License has been removed"
                 Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -RemoveLicenses $group.description
             }
         } 
